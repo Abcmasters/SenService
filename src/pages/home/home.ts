43574359ff47525934422bb2a3733c04ea633816
Abcmasters <38254556+Abcmasters@users.noreply.github.com>
2018-04-15 21:14:13 +0200

    Es werden die Benutzerdaten an die home.html weitergegeben und testweise ausgegeben.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RegistrierungPage} from "../registrierung/registrierung";
+import {NetworkEngineProvider} from "../../providers/network-engine/network-engine";
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+bname:any;
+vname:any;
+nname:any;
+email:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
   }
-
+    ionViewDidLoad() { this.bname = this.navParams.get('Benutzername');this.nname = this.navParams.get('Nachname');this.nname = this.navParams.get('Vorname');this.email = this.navParams.get('Mail'); }
 }
